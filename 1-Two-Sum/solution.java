d03df9d666abfd31d814bae6aadfc7fb1ab775e3
adam lee <adambillylee@gmail.com>
2016-05-02 16:35:41 -0700

    [1. Two Sum][Accepted]committed by LeetCode Extension

--- 1-Two-Sum/solution.java
+++ 1-Two-Sum/solution.java
@@ -6,11 +6,14 @@ public class Solution {
         for (int i=0; i<nums.length; i++) {
             int n = nums[i];
             
+            // if target - n is in map, construct result aray
             if (map.containsKey(target - n)) {
                 result[0] = map.get(target - n);
                 result[1] = i;
                 return result;
             }else{
+                // if target - n is not in map, put n as key and index as value
+                // do not get confused and put target - n into map
                 map.put(n, i);
             }
         }
