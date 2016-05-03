e5c724eb6dfaf69a63768981e55a7ec0c8f7f74e
adam lee <adambillylee@gmail.com>
2016-05-02 22:01:21 -0700

    [15. 3Sum][Time Limit Exceeded]committed by LeetCode Extension

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -5,12 +5,11 @@ public class Solution {
 
         List<List<Integer>> rst = new ArrayList<List<Integer>>();
 
-        int i = 0;
-
-        while (i < nums.length - 2) {
+        for (int i=0; i<nums.length; i++) {
             // if i is in dep sequence, move i to last element
-            if (i - 1 >= 0 && nums[i] == nums[i - 1])
+            if (i - 1 >= 0 && nums[i] == nums[i - 1]) {
                 continue;
+            }
 
             int j = i + 1;
             int k = nums.length - 1;
@@ -28,6 +27,9 @@ public class Solution {
                     // if j is in dup sequence, move k to the first of dups > j
                     while (k - 1 < j && nums[k] == nums[k - 1])
                         k--;
+                    
+                    j++;
+                    j--;
                 } else if (sum < 0) {
                     // move j forward (to k / to non-dup element)
                     j++;
@@ -36,8 +38,6 @@ public class Solution {
                     k--;
                 }
             }
-
-            i++;
         }
 
         return rst;
