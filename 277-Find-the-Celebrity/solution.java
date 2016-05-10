bce3bfbaa778cd53e4e836a4bef1ee1319ac9dc3
adam lee <adambillylee@gmail.com>
2016-05-09 23:50:18 -0700

    [277. Find the Celebrity][Compile Error]committed by LeetCode Extension

--- 277-Find-the-Celebrity/solution.java
+++ 277-Find-the-Celebrity/solution.java
@@ -2,27 +2,31 @@
       boolean knows(int a, int b); */
 
 public class Solution extends Relation {
-    public int findCelebrity(int n) {
-        int celebrity = 0;
-        
-        for (int i=1; i<n; i++) {
-            if (knows(celebrity, i))
-                celebrity = i;
-        }
-        
-        int i=0;
-        for (i<n) {
-            if (i != celebrity || (knows(i, celebrity) && !knows(celebrity, i)))
-                System.out.println(i);
+    public class Solution extends Relation {
+        public int findCelebrity(int n) {
+            int celebrity = 0;
+
+            for (int i=1; i<n; i++) {
+                if (knows(celebrity, i)) {
+                    celebrity = i;
+                }
+            }
+
+            int i = 0;
+            while (i<n) {
+                if (i == celebrity || (!knows(celebrity, i) && knows(i, celebrity))) {
+                    System.out.println(i);
+                }
+                else {
+                    break;
+                }
+                i++;
+            }
+
+            if (i == n)
+                return celebrity;
             else
-                break;
-                
-            i++;
+                return -1;
         }
-        
-        if (i == n)
-            return celebrity;
-        else
-            return -1;
     }
 }
