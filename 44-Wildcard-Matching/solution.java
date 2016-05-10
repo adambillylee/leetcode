73ac3c30ff79b3bd6ed1640859f47786b3140063
adam lee <adambillylee@gmail.com>
2016-05-10 00:16:35 -0700

    [44. Wildcard Matching][Runtime Error]committed by LeetCode Extension

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -1,11 +1,12 @@
 public class Solution {
-    boolean dp[][];
+    int dp[][];
     
     public boolean isMatch(String s, String p) {
-        if (s == null || p == null || s.length() == 0 || p.length() == 0)
+        if (s == null || p == null || 
+        s.length() == 0 || p.length() == 0)
             return false;
         
-        dp = new boolean[s.length()][p.length()];
+        dp = new int[s.length()][p.length()];
         for (int i=0; i<s.length(); i++) {
             for (int j=0; j<p.length(); j++) {
                 dp[i][j] = -1;
@@ -17,36 +18,36 @@ public class Solution {
     }
     
     public boolean helper(String s, String p, int indS, int indP) {
+        if (indP == p.length())
+            return indS == s.length();
+            
         if (dp[indS][indP] == 1)
             return true;
             
         if (dp[indS][indP] == 0)
             return false;
-        
-        if (indP == p.length())
-            return indS == s.length();
             
         if (p.charAt(indP) != '*') {
             if (!match(s, p, indS, indP)) {
-                dp[indS][indP] == 0;
+                dp[indS][indP] = 0;
                 return false;
             }else{
                 return helper(s, p, indS+1, indP+1);
             }
         }else{
             if (helper(s, p, indS, indP+1)) {
-                dp[indS][indP] == 1;
+                dp[indS][indP] = 1;
                 return true;
             }
                 
             while (match(s, p, indS, indP)) {
                 if (helper(s, p, ++indS, indP+1)) {
-                    dp[indS][indP] == 1;
+                    dp[indS][indP] = 1;
                     return true;
                 }
             }
             
-            dp[indS][indP] == 0;
+            dp[indS][indP] = 0;
             return false;
         }
     }
