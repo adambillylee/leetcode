bd9eba19a34d7f59495754631e331ef2c3fb96cb
adam lee <adambillylee@gmail.com>
2016-05-09 23:50:48 -0700

    [277. Find the Celebrity][Accepted]committed by LeetCode Extension

--- 277-Find-the-Celebrity/solution.java
+++ 277-Find-the-Celebrity/solution.java
@@ -2,31 +2,29 @@
       boolean knows(int a, int b); */
 
 public class Solution extends Relation {
-    public class Solution extends Relation {
-        public int findCelebrity(int n) {
-            int celebrity = 0;
-
-            for (int i=1; i<n; i++) {
-                if (knows(celebrity, i)) {
-                    celebrity = i;
-                }
+    public int findCelebrity(int n) {
+        int celebrity = 0;
+    
+        for (int i=1; i<n; i++) {
+            if (knows(celebrity, i)) {
+                celebrity = i;
             }
-
-            int i = 0;
-            while (i<n) {
-                if (i == celebrity || (!knows(celebrity, i) && knows(i, celebrity))) {
-                    System.out.println(i);
-                }
-                else {
-                    break;
-                }
-                i++;
-            }
-
-            if (i == n)
-                return celebrity;
-            else
-                return -1;
         }
+    
+        int i = 0;
+        while (i<n) {
+            if (i == celebrity || (!knows(celebrity, i) && knows(i, celebrity))) {
+                System.out.println(i);
+            }
+            else {
+                break;
+            }
+            i++;
+        }
+    
+        if (i == n)
+            return celebrity;
+        else
+            return -1;
     }
 }
