2b1a7535d7dfc9ec469ee73bd97da698cdf516aa
adam lee <adambillylee@gmail.com>
2016-05-12 22:39:07 -0700

    [320. Generalized Abbreviation][Accepted]committed by LeetCode Extension

--- 320-Generalized-Abbreviation/solution.java
+++ 320-Generalized-Abbreviation/solution.java
@@ -7,28 +7,37 @@ public class Solution {
             return rst;
         }
 
-        helper(rst, word, 0, 0, "");
+        helper(rst, word, 0, new StringBuilder(), false);
 
         return rst;
     }
 
-    private void helper(List<String> rst, String word, int index, int count, String curr) {
+    private void helper(List<String> rst, String word, int index, StringBuilder sb, boolean prevNum) {
         if (index == word.length()) {
-            if (count > 0) {
-                rst.add(curr + count);
-            } else {
-                rst.add(curr);
-            }
+            rst.add(sb.toString());
             return;
         }
 
-        helper(rst, word, index + 1, count + 1, curr);
+        int len = sb.length();
 
-        if (count > 0) {
-            helper(rst, word, index + 1, 0, curr + count + word.charAt(index));
-        } else {
-            helper(rst, word, index + 1, 0, curr + word.charAt(index));
+        /**
+         * loop from current index to end of word
+         */
+        for (int i = index; i < word.length(); i++) {
+            if (i == index) {
+                helper(rst, word, i + 1, sb.append(word.charAt(i)), false);
+                sb.setLength(len);
+            }
+
+            if (!prevNum) {
+                helper(rst, word, i + 1, sb.append(getNumberString(index, i)), true);
+                sb.setLength(len);
+            }
         }
+    }
 
+    private String getNumberString(int start, int end) {
+        // if start == end, the number is 1
+        return Integer.toString(end - start + 1);
     }
 }
