980f214db2c9d907338d939f384059207b77db46
adam lee <adambillylee@gmail.com>
2016-05-12 22:56:09 -0700

    [78. Subsets][Compile Error]committed by LeetCode Extension

---
+++ 78-Subsets/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> rst = new ArrayList<Integer>();
+        
+        if (nums.size == 0)
+            return rst;
+            
+        helper(rst, new ArrayList<Integer>(), nums, 0);
+        
+        return rst;
+    }
+    
+    public void helper(List<List<Integer> rst, List<Integer> list, int[] nums, int index) {
+        if (index == nums.length) {
+            rst.add(new ArrayList(list));
+            return;
+        }
+        
+        for (int i=index; i<nums.length; i++) {
+            // add current element
+            list.add(nums[i]);
+            helper(rst, list, nums, index+1);
+            list.remove(list.size()-1);
+            
+            // not add current element
+            helper(rst, list, nums, index+1);
+        }
+    }
+}
