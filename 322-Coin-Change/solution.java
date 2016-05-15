eaedd2a21b3bdeac0a9d7dca05f1791badd15733
adam lee <adambillylee@gmail.com>
2016-05-15 15:24:57 -0700

    [322. Coin Change][Accepted]committed by LeetCode Extension

--- 322-Coin-Change/solution.java
+++ 322-Coin-Change/solution.java
@@ -1,37 +1,23 @@
 public class Solution {
     public int coinChange(int[] coins, int amount) {
         if (amount == 0)
-            return -1;
+            return 0;
 
         int dp[] = new int[amount + 1];
         // initiate dp
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
-            dp[i] = -1;
+            dp[i] = Integer.MAX_VALUE;
         }
 
-        // make a set of coins to achieve O(1) search time
-        Set<Integer> set = new HashSet<>();
-        for (int coin : coins)
-            set.add(coin);
-
         for (int i = 0; i <= amount; i++) {
-            for (int j = 0; j <i; j++) {
-                /**
-                 * if there is a way to make up j and there is coin to add j up to i
-                 */
-                if (dp[j] > -1 && set.contains(i - j)) {
-                    // if this is the first combination for i, update it with dp[j]+1
-                    if (dp[i] == -1) {
-                        dp[i] = dp[j] + 1;
-                    } else {
-                        // if there are other combination for i already, update it with the min
-                        dp[i] = Math.min(dp[i], dp[j] + 1);
-                    }
+            for (int coin : coins) {
+                if (i >= coin && dp[i - coin] < Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
 
-        return dp[amount];
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 }
