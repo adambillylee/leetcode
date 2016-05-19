cc69441686e889f53fd7b8103ba80da41b0e247e
adam lee <adambillylee@gmail.com>
2016-05-19 11:27:38 -0700

    [91. Decode Ways][Accepted]committed by LeetCode Extension

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -1,35 +1,38 @@
 public class Solution {
     public int numDecodings(String s) {
-        int dp[] = new int[s.length()+1];
-        
+        if (s.isEmpty())
+            return 0;
+
+        int dp[] = new int[s.length() + 1];
+
         dp[0] = 1;
-        for (int i=1; i<s.length()-1;i++) {
-            for (int j=i-1; j>=i-2; j++) {
-                if (j-1<=0)
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = i - 1; j >= i - 2; j--) {
+                if (j < 0)
                     continue;
-                
-                String curr = s.substring(j,i);
-        
-                if (dp[j]>1 && valid(curr))
-                    dp[i] = dp[j]+1;
+
+                String curr = s.substring(j, i);
+
+                if (dp[j] > 0 && valid(curr))
+                    dp[i] += dp[j];
             }
         }
-        
+
         return dp[s.length()];
     }
-    
+
     boolean valid(String curr) {
         if (curr.isEmpty())
             return false;
-            
+
         if (curr.charAt(0) == '0')
             return false;
-        
+
         int number = Integer.parseInt(curr);
-        
-        if (number <0 || number >26)
+
+        if (number < 0 || number > 26)
             return false;
-            
+
         return true;
     }
 }
