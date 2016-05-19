00208170e884919999317baee07f97ddfeb65e49
adam lee <adambillylee@gmail.com>
2016-05-19 10:31:30 -0700

    [91. Decode Ways][Compile Error]committed by LeetCode Extension

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -1,10 +1,10 @@
 public class Solution {
     public int numDecodings(String s) {
-        int dp = new int[s.length()+1];
+        int dp[] = new int[s.length()+1];
         
         dp[0] = 1;
         for (int i=0; i<s.length()-1;i++) {
-            for (int j=i-1; j>=i=2; j++) {
+            for (int j=i-1; j>=i-2; j++) {
                 int curr = Integer.parseInt(s.substring(j,i));
         
                 if (dp[j]>1 && curr >0 && dp <=26)
