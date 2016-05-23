451054372afa0db9c37665f04651d5bd8220aba8
adam lee <adambillylee@gmail.com>
2016-05-23 08:07:40 -0700

    [141. Linked List Cycle][Compile Error]committed by LeetCode Extension

---
+++ 141-Linked-List-Cycle/solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (head.next != null || head.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (slow.val = fast.val)
+                return false;
+        }
+        
+        return true;
+    }
+}
