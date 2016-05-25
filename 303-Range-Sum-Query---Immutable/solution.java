fd5b9ade0d1dafe89e526ce32991f77185558228
adam lee <adambillylee@gmail.com>
2016-05-25 16:56:21 -0700

    [303. Range Sum Query - Immutable][Compile Error]committed by LeetCode Extension

---
+++ 303-Range-Sum-Query---Immutable/solution.java
@@ -0,0 +1,23 @@
+public class NumArray {
+
+    int sum[];
+
+    public RangeSumQuery(int[] nums) {
+        sum = new int[nums.length];
+
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[i - 1] - sum[j];
+    }
+}
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray = new NumArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.sumRange(1, 2);
