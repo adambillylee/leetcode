a2bab59a980b98876151eb0368048638f69e2c48
adam lee <adambillylee@gmail.com>
2016-05-26 23:25:20 -0700

    [338. Counting Bits][Time Limit Exceeded]committed by LeetCode Extension

--- 338-Counting-Bits/solution.java
+++ 338-Counting-Bits/solution.java
@@ -2,15 +2,15 @@ public class Solution {
     public int[] countBits(int num) {
         int rst[] = new int[num + 1];
         rst[0] = 0;
-        rst[1] = 1;
 
         int pow = 1;
-        for (int i = 2; i < num; i++) {
+        for (int i = 1; i <= num; i++) {
             if (i == pow * 2) {
                 pow *= 2;
             }
 
             rst[i] = rst[i - pow] + 1;
+            System.out.println(i + " : " + rst[i]);
         }
 
         return rst;
