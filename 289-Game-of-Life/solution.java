02d2b1ab5c6ee2ef5abad7c6cfd55fcbe024bc82
adam lee <adambillylee@gmail.com>
2016-05-31 23:54:12 -0700

    [289. Game of Life][Accepted]committed by LeetCode Extension

--- 289-Game-of-Life/solution.java
+++ 289-Game-of-Life/solution.java
@@ -9,38 +9,50 @@ public class Solution {
         if (m == 0 || n == 0)
             return;
 
-        int[][] rst = new int[m][n];
-
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 board[i][j] = calculate(board, i, j);
+//                System.out.print(board[i][j] + " ");
+            }
+//            System.out.println();
+        }
+
+//        System.out.println();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] >>= 1;
             }
         }
     }
 
     private int calculate(int[][] board, int i, int j) {
+        int total = 0;
+
+        total = addValue(board, i - 1, j - 1) + addValue(board, i - 1, j) + addValue(board, i, j - 1)
+                + addValue(board, i + 1, j + 1) + +addValue(board, i, j + 1) + addValue(board, i + 1, j)
+                + addValue(board, i + 1, j - 1) + addValue(board, i - 1, j + 1);
+
+        if ((board[i][j] & 1) == 0) {
+            if (total == 3)
+                return 2;
+            else
+                return 0;
+        } else {
+            if (total < 2 || total > 3)
+                return 1;
+            else
+                return 3;
+        }
+    }
+
+    private int addValue(int[][] board, int i, int j) {
         if (i < 0 || j < 0)
             return 0;
 
         if (i >= m || j >= n)
             return 0;
 
-        int curr = board[i][j];
-
-        int neighbours = calculate(board, i + 1, j + 1) + calculate(board, i + 1, j) + calculate(board, i, j + 1) +
-                calculate(board, i - 1, j - 1) + calculate(board, i - 1, j) + calculate(board, i, j - 1) +
-                calculate(board, i + 1, j - 1) + calculate(board, i - 1, j + 1);
-
-        if (curr == 1) {
-            if (neighbours == 2 || neighbours == 3)
-                return 1;
-            else
-                return 0;
-        } else {
-            if (neighbours == 3)
-                return 1;
-            else
-                return 0;
-        }
+        return board[i][j] & 1;
     }
 }
