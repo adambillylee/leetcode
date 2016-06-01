aaaf1789fe7bd5331713252a17207bcff6d4bfe0
adam lee <adambillylee@gmail.com>
2016-05-31 21:51:57 -0700

    [289. Game of Life][Runtime Error]committed by LeetCode Extension

---
+++ 289-Game-of-Life/solution.java
@@ -0,0 +1,52 @@
+public class Solution {
+    int m;
+    int n;
+
+    public void gameOfLife(int[][] board) {
+        m = board.length;
+        n = board[0].length;
+
+        if (m == 0 || n == 0)
+            return;
+
+        int[][] rst = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rst[i][j] = calculate(board, i, j);
+            }
+        }
+
+        board = rst;
+    }
+
+    private int calculate(int[][] board, int i, int j) {
+        int total = 0;
+
+        total = addValue(board, i - 1, j - 1) + addValue(board, i - 1, j) + addValue(board, i, j - 1)
+                + addValue(board, i + 1, j + 1) + +addValue(board, i, j + 1) + addValue(board, i + 1, j)
+                + addValue(board, i + 1, j - 1) + addValue(board, i - 1, j + 1);
+
+        if (board[i][j] == 0) {
+            if (total == 3)
+                return 1;
+            else
+                return 0;
+        } else {
+            if (total == 2 || total == 3)
+                return 1;
+            else
+                return 0;
+        }
+    }
+
+    private int addValue(int[][] board, int i, int j) {
+        if (i < 0 || j < 0)
+            return 0;
+
+        if (i > m || j > n)
+            return 0;
+
+        return board[i][j];
+    }
+}
