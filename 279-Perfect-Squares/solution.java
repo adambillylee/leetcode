b0bf9ab81192f633e76144c4a826f6dc105c0660
adam lee <adambillylee@gmail.com>
2016-06-02 21:04:19 -0700

    [279. Perfect Squares][Accepted]committed by LeetCode Extension

---
+++ 279-Perfect-Squares/solution.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public int numSquares(int n) {
+        if (n < 0)
+            return 0;
+
+        if (n < 1)
+            return n;
+
+        // largest sqr which its square <= n
+        int sqr[] = new int[n + 1];
+
+        // min number of perfect square adds up to n
+        int min[] = new int[n + 1];
+
+        sqr[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            int nextSq = (sqr[i - 1] + 1) * (sqr[i - 1] + 1);
+
+            if (nextSq > i) {
+                sqr[i] = sqr[i - 1];
+                min[i] = i;
+
+                for (int j = 1; j * j < i; j++) {
+                    int tmp = min[i - j * j] + min[j * j];
+
+                    if (tmp < min[i])
+                        min[i] = tmp;
+                }
+            } else {
+                sqr[i] = sqr[i - 1] + 1;
+                min[i] = 1;
+            }
+        }
+
+        return min[n];
+    }
+}
