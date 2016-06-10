946e5ea9f1f07435503365af5642bfdc8fd4646d
adam lee <adambillylee@gmail.com>
2016-06-10 00:17:02 -0700

    [180. Consecutive Numbers][Runtime Error]committed by LeetCode Extension

--- 180-Consecutive-Numbers/solution.sql
+++ 180-Consecutive-Numbers/solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select distinct(l1.Num)
 from Logs l1, Logs l2, Logs l3
-where l2.Id - l3.Id = 1 and l3.Id - l2.Id = 1
+where l1.Id=l2.Id-1 and l2.Id=l3.Id-1 
 and l1.Num = l2.Num and l2.Num = l3.Num;
