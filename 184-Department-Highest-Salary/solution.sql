ce83a402e9d3e147ca4f378d438208f787d6df8d
adam lee <adambillylee@gmail.com>
2016-06-14 01:30:37 -0700

    [184. Department Highest Salary][Runtime Error]committed by LeetCode Extension

--- 184-Department-Highest-Salary/solution.sql
+++ 184-Department-Highest-Salary/solution.sql
@@ -1,4 +1,4 @@
-select `Department.Name`, `Employee.Name`, (select DepartmentId, Max(Salary) as max from Employee group by DepartmentId) T;
+select `Department`.`Name`, `Employee`.`Name`, (select DepartmentId, Max(Salary) as max from Employee group by DepartmentId) T;
 from Department d
 join Employee e
 on Employee.DepartmentId = Department.Id
