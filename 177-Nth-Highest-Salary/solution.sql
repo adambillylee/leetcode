2625d5aa2743dc9232791bf21ca01dcc69cbbd7a
adam lee <adambillylee@gmail.com>
2016-06-13 22:32:23 -0700

    [177. Nth Highest Salary][Accepted]committed by LeetCode Extension

--- 177-Nth-Highest-Salary/solution.sql
+++ 177-Nth-Highest-Salary/solution.sql
@@ -7,6 +7,6 @@ BEGIN
       select e1.Salary
       from (select distinct(Salary) from Employee) e1
       where (select count(*) from (select distinct(Salary) from Employee) e2 
-        where e2.Salary > e1.Salary) = N-1;
+        where e2.Salary > e1.Salary) = N-1
   );
 END
