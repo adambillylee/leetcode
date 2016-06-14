01be96971a9d1682559932d37cf7287a35a23015
adam lee <adambillylee@gmail.com>
2016-06-13 23:58:25 -0700

    [178. Rank Scores][Runtime Error]committed by LeetCode Extension

--- 178-Rank-Scores/solution.sql
+++ 178-Rank-Scores/solution.sql
@@ -1,3 +1,3 @@
-select Score, (select count(distinct(Score)) from Scores s1 where s1.Score > score) as Rank
+select Score, (select count(distinct(Score)) from Scores s1 where s1.Score >= Score) as Rank
 from Scores
 order by Scores;
