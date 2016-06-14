1be8535c6a7a6ab29088ee13811563c4daf28b72
adam lee <adambillylee@gmail.com>
2016-06-13 22:30:41 -0700

    [177. Nth Highest Salary][Runtime Error]committed by LeetCode Extension

--- 177-Nth-Highest-Salary/solution.sql
+++ 177-Nth-Highest-Salary/solution.sql
@@ -4,9 +4,9 @@ BEGIN
   SET M = N-1;
   RETURN (
       # Write your MySQL query statement below.
-      select distinct(Salary)
-      from Employee e1
-      where select count(Salary) from 
-        (select distinct(Salary) from Employee e2 where e2.Salary > e1.Salary) = N-1;
+      select e1.Salary
+      from select distinct(Salary) from Employee e1
+      where (select count(*) from (select distinct(Salary) from Employee) e2 
+        where e2.Salary > e1.Salary) = N-1;
   );
 END
