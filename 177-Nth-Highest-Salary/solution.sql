6a214b000e0daebd7ccc6e440b322ece05e3144c
adam lee <adambillylee@gmail.com>
2016-06-13 17:04:02 -0700

    [177. Nth Highest Salary][Runtime Error]committed by LeetCode Extension

---
+++ 177-Nth-Highest-Salary/solution.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET M = N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      select Max(Salary)
+      from Employee
+      order by Salary
+      limit M, 1;
+  );
+END
