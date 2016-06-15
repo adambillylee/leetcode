0d5a63bc475ca84d6c840bc76bd42621453a5dba
adam lee <adambillylee@gmail.com>
2016-06-14 21:59:25 -0700

    [263. Ugly Number][Compile Error]committed by LeetCode Extension

---
+++ 263-Ugly-Number/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean isUgly(int num) {
+        if (num < 1)
+            return false;
+
+        int factors[] = {2,3,5};
+
+        for (int factor : factors) {
+            while (num % factor == 0) {
+                num = num / factor;
+            }
+        }
+
+        return num == 1;
+    }
+    }
+}
