0b4c4a056e1b1cbb4ae3aa10563094de14ae6285
adam lee <adambillylee@gmail.com>
2016-06-16 17:01:23 -0700

    [185. Department Top Three Salaries][Runtime Error]committed by LeetCode Extension

--- 185-Department-Top-Three-Salaries/solution.sql
+++ 185-Department-Top-Three-Salaries/solution.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 select d.Name as Department, e.Name as Employee, s1.Salary as Salary
-from (select (count(distinct(Salary)))
+from Employee s1
+where (select (count(distinct(Salary)))
         from Employee e2
         where e2.Salary > e2.Salary
         and e2.departmentId = e1.departmentId
