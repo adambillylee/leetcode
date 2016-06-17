5b478b45e2d43194e23219f2fc2402b8890fde97
adam lee <adambillylee@gmail.com>
2016-06-16 17:04:24 -0700

    [185. Department Top Three Salaries][Accepted]committed by LeetCode Extension

--- 185-Department-Top-Three-Salaries/solution.sql
+++ 185-Department-Top-Three-Salaries/solution.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
-select d.Name as Department, e1.Name as Employee, s1.Salary as Salary
+select d.Name as Department, e1.Name as Employee, e1.Salary as Salary
 from Employee e1
 join Department d
 on e1.departmentId = d.Id
-where (select (count(distinct(Salary)))
+where (select (count(distinct(e2.Salary)))
         from Employee e2
-        where e2.Salary > e2.Salary
+        where e2.Salary > e1.Salary
         and e2.departmentId = e1.departmentId
 ) < 3;
