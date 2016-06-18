88db91f82797708f5edf711fe64de0973548ee78
adam lee <adambillylee@gmail.com>
2016-06-18 00:22:21 -0700

    [200. Number of Islands][Runtime Error]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -1,20 +1,20 @@
 public class Solution {
-    char [][] guid;
+    char [][] grid;
     int m;
     int n;
     int counter = 0;
     
     public int numIslands(char[][] grid) {
-        if (guid.length == 0 || guid[0].length == 0)
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
             return 0;
             
-        this.guid = guid;
-        this.m = guid.length;
-        this.n = guid[0].length;
+        this.grid = grid;
+        this.m = grid.length;
+        this.n = grid[0].length;
         
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
-                if (guid[i][j] == '1') {
+                if (grid[i][j] == '1') {
                     helper(i, j);
                     counter++;
                 }
@@ -31,10 +31,10 @@ public class Solution {
         if (i>m || j>n)
             return;
             
-        if (guid[i][j] == '2' || guid[i][j] == '0')
+        if (grid[i][j] == '2' || grid[i][j] == '0')
             return;
             
-        guid[i][j] = '2';
+        grid[i][j] = '2';
             
         helper(i+1, j);
         helper(i, j+1);
