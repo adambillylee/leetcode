cc4b88d596800431417fe4cfe422750d940d01a4
adam lee <adambillylee@gmail.com>
2016-06-19 11:25:49 -0700

    [320. Generalized Abbreviation][Compile Error]committed by LeetCode Extension

--- 320-Generalized-Abbreviation/solution.java
+++ 320-Generalized-Abbreviation/solution.java
@@ -1,43 +1,36 @@
 public class Solution {
     public List<String> generateAbbreviations(String word) {
-        List<String> rst = new ArrayList<>();
-
-        if (word.length() == 0) {
-            rst.add("");
+        List<String> rst = new ArrayList();
+        
+        if (word == null)
             return rst;
-        }
-
+            
         helper(rst, word, 0, new StringBuilder(), false);
-
         return rst;
     }
-
-    private void helper(List<String> rst, String word, int index, StringBuilder sb, boolean prevNum) {
+    
+    private void helper(List<String> rst, String word, int index, StringBuilder sb, boolean preNum) {
         if (index == word.length()) {
             rst.add(sb.toString());
             return;
         }
-
+            
         int len = sb.length();
-
-        /**
-         * loop from current index to end of word
-         */
-        for (int i = index; i < word.length(); i++) {
+        
+        for (int i=index; i<word.length; i++) {
             if (i == index) {
-                helper(rst, word, i + 1, sb.append(word.charAt(i)), false);
+                helper(rst, word, i+1, sb.append(word.charAt(i)), false);
                 sb.setLength(len);
             }
-
-            if (!prevNum) {
-                helper(rst, word, i + 1, sb.append(getNumberString(index, i)), true);
+            
+            if (preNum = false) {
+                helper(rst, word, i+1, sb.append(getNumber(i, index)), true);
                 sb.setLength(len);
             }
         }
     }
-
-    private String getNumberString(int start, int end) {
-        // if start == end, the number is 1
-        return Integer.toString(end - start + 1);
+    
+    private String getNumber(int i, int index) {
+        return Integer.toString(i - index + 1);
     }
 }
