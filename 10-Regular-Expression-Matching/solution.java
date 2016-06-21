417064b1453691b7e9857398c6a53f5900ce851f
adam lee <adambillylee@gmail.com>
2016-06-21 15:50:34 -0700

    [10. Regular Expression Matching][Time Limit Exceeded]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,35 +1,71 @@
 public class Solution {
-    public boolean isMatch(String s, String p) {
-        return helper(s, p, 0, 0);
-    }
+    class Pair {
+        int indS;
+        int indP;
 
-    private boolean helper(String s, String p, int indS, int indP) {
-        if (indP == p.length())
-            return indS == s.length();
-
-        if (indP + 1 >= p.length() || p.charAt(indP + 1) != '*') {
-            if (!matchFirst(s, p, indS, indP))
-                return false;
-            else
-                return helper(s, p, indS + 1, indP + 1);
-        } else {
-
-            if (helper(s, p, indS, indP + 2))
-                return true;
-
-            while (matchFirst(s, p, indS, indP)) {
-                if (helper(s, p, ++indS, indP + 2))
-                    return true;
-            }
-
-            return false;
+        Pair (int indS, int indP) {
+            this.indS = indS;
+            this.indP = indP;
         }
     }
 
-    private boolean matchFirst(String s, String p, int indS, int indP) {
-        if (indS >= s.length())
-            return false;
+    public boolean isMatch(String s, String p) {
+        Map<Pair, Boolean> map = new HashMap<>();
 
-        return (s.charAt(indS) == p.charAt(indP)) || (p.charAt(indP) == '.');
+        return helper(p, s, map);
+    }
+
+    private boolean helper(String p, String s, Map<Pair, Boolean> map) {
+        Pair curr = new Pair(s.length(), p.length());
+
+        if (map.containsKey(curr))
+            return map.get(p);
+
+        if (p.isEmpty() && s.isEmpty()) {
+            map.put(curr, true);
+            return true;
+        }
+
+        if (p.length() == 2 && p.charAt(1) == '*' && s.isEmpty()) {
+            map.put(curr, true);
+            return true;
+        }
+
+        if (p.isEmpty()) {
+            map.put(curr, false);
+            return false;
+        }
+
+        if (s.isEmpty()) {
+            map.put(curr, false);
+            return false;
+        }
+
+        boolean rst = false;
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            String pHead = p.substring(0,1);
+            String remaining = p.substring(2);
+
+            if (helper(remaining, s, map))
+                rst = true;
+            else {
+                while (!s.isEmpty() && (s.startsWith(pHead) || p.charAt(0) == '.')) {
+                    rst = helper(remaining, s.substring(1), map);
+
+                    if (rst)
+                        break;
+
+                    s = s.substring(1);
+                }
+            }
+        } else {
+            if (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.')
+                rst = helper(p.substring(1), s.substring(1), map);
+            else
+                rst = false;
+        }
+
+        map.put(curr, rst);
+        return rst;
     }
 }
