709ab196ccd5a953e32a956c3c7029e7e3e69481
adam lee <adambillylee@gmail.com>
2016-06-21 16:15:47 -0700

    [10. Regular Expression Matching][Accepted]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,15 +1,21 @@
+/**
+ * Created by adamli on 6/21/16.
+ */
 public class Solution {
     public boolean isMatch(String s, String p) {
         return helper(p, s);
     }
 
     private boolean helper(String p, String s) {
-
         if (p.isEmpty() && s.isEmpty()) {
             return true;
         }
 
-        if (p.length() >= 2 && p.charAt(1) == '*' && s.isEmpty()) {
+        /**
+         * s = ""
+         * p = "*c*c"
+         */
+        if (hasStar(p) && s.isEmpty()) {
             return helper(p.substring(2), s);
         }
 
@@ -21,29 +27,56 @@ public class Solution {
             return false;
         }
 
-        boolean rst = false;
-        if (p.length() >= 2 && p.charAt(1) == '*') {
-            String remaining = p.substring(2);
-
-            if (helper(remaining, s))
-                rst = true;
+        if (hasStar(p)) {
+            /**
+             * when star match nothing
+             */
+            if (helper(p.substring(2), s))
+                return true;
             else {
-                while (!s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.')) {
-                    rst = helper(remaining, s.substring(1));
-
-                    if (rst)
-                        break;
-
-                    s = s.substring(1);
+                /**
+                 * when star match at least one char in s
+                 */
+                while (!s.isEmpty() && matchHead(p, s)) {
+                    /**
+                     * BUG POINT: remove current matching first char in s
+                     */
+                    if (helper(p.substring(2), s.substring(1)))
+                        return true;
+                    else
+                        s = s.substring(1);
                 }
             }
         } else {
-            if (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.')
-                rst = helper(p.substring(1), s.substring(1));
-            else
-                rst = false;
+            if (matchHead(p, s))
+                return helper(p.substring(1), s.substring(1));
         }
 
-        return rst;
+        return false;
+    }
+
+    /**
+     * is current p head a star pattern? (.* or a*, etc)
+     * @param p
+     * @return true or false
+     */
+    private boolean hasStar(String p) {
+        return p.length() >= 2 && p.charAt(1) == '*';
+    }
+
+    /**
+     * does the first char in s and p match?
+     * @param p
+     * @param s
+     * @return true or false
+     */
+    private boolean matchHead(String p, String s) {
+        if (s.charAt(0) == p.charAt(0))
+            return true;
+
+        if (p.charAt(0) == '.')
+            return true;
+
+        return false;
     }
 }
