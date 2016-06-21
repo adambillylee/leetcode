ea32d7c8fa4657735e1f18691a3a13690639e51e
adam lee <adambillylee@gmail.com>
2016-06-21 00:34:05 -0700

    [139. Word Break][Accepted]committed by LeetCode Extension

---
+++ 139-Word-Break/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        if (s == null || wordDict == null || wordDict.size() == 0)
+            return false;
+
+        int len = s.length();
+
+        // up to ith char in s is separable in wordDict
+        boolean dp[] = new boolean[len + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j]) {
+                    String curr = s.substring(j, i);
+
+                    if (wordDict.contains(curr)) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
