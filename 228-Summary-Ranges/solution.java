77078c1bfb69f20059cf86620dedd09381ee3d56
adam lee <adambillylee@gmail.com>
2016-06-29 16:22:31 -0700

    [228. Summary Ranges][Accepted]committed by LeetCode Extension

---
+++ 228-Summary-Ranges/solution.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> rst = new ArrayList<>();
+        if (nums == null || nums.length == 0)
+            return rst;
+
+        if (nums.length == 1) {
+            rst.add(String.valueOf(nums[0]));
+            return rst;
+        }
+
+        int start = nums[0];
+        int end = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == end + 1) {
+                end = nums[i];
+            } else {
+                addResult(rst, start, end);
+                start = nums[i];
+                end = nums[i];
+            }
+        }
+
+        addResult(rst, start, end);
+
+        return rst;
+    }
+
+    private void addResult(List<String> rst, int start, int end) {
+        if (start == end) {
+            rst.add(String.valueOf(start));
+        } else {
+            rst.add(start + "->" + end);
+        }
+    }
+}
