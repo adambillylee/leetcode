532a3cc4cb56bde09f49a0331650ecf2b8b5260f
adam lee <adambillylee@gmail.com>
2016-06-29 16:59:44 -0700

    [163. Missing Ranges][Accepted]committed by LeetCode Extension

---
+++ 163-Missing-Ranges/solution.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        List<String> rst = new ArrayList<>();
+        if (nums.length == 0 || nums == null) {
+            addResult(rst, lower, upper);
+            return rst;
+        }
+
+        if (nums[0] > lower) {
+            int start = lower;
+            int end = nums[0] - 1;
+
+            addResult(rst, start, end);
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] != 1) {
+                int start = nums[i - 1] + 1;
+                int end = nums[i] - 1;
+                addResult(rst, start, end);
+            }
+        }
+
+        if (nums[nums.length - 1] < upper) {
+            int start = nums[nums.length - 1] + 1;
+            int end = upper;
+            addResult(rst, start, end);
+        }
+
+        return rst;
+    }
+
+    private void addResult(List<String> rst, int start, int end) {
+        if (start == end)
+            rst.add(String.valueOf(start));
+        else {
+            rst.add(start + "->" + end);
+        }
+    }
+}
