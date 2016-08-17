d62a71be0e50bbebfb722545e296d39c191c550d
adam lee <adambillylee@gmail.com>
2016-08-17 00:49:46 -0700

    [23. Merge k Sorted Lists][Accepted]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.java
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -11,23 +11,60 @@ public class Solution {
         if (lists == null || lists.length == 0)
             return null;
 
+        if (lists.length == 1)
+            return lists[0];
+
+        // convert array into list for manipulation
+        List<ListNode> list = new ArrayList();
+        for (ListNode ele : lists)
+            list.add(ele);
+
+        while (list.size() > 1) {
+            List<ListNode> next = new ArrayList();
+
+            // merge listNodes pair by pair and add result into next iteration
+            for (int i = 0; i < list.size() - 1; i += 2) {
+                ListNode merged = mergeTwo(list.get(i), list.get(i + 1));
+                next.add(merged);
+            }
+
+            // if list length is even, the last one would not be merged, add it into next iteration as well
+            if (list.size() % 2 == 1) {
+                next.add(0, list.get(list.size() - 1));
+            }
+
+            list = next;
+        }
+        return list.get(0);
+    }
+
+    // merge two sorted linked lists
+    ListNode mergeTwo(ListNode head1, ListNode head2) {
         ListNode dummy = new ListNode(-1);
         ListNode curr = dummy;
 
-        PriorityQueue<ListNode> heap = new PriorityQueue<>((n1, n2)->(n1.val - n2.val));
+        while (head1 != null || head2 != null) {
+            // if one of the input head is null, add the other into rst
+            if (head1 == null) {
+                curr.next = head2;
+                break;
+            }
 
-        for (ListNode tmp : lists) {
-            if (tmp != null)
-                heap.add(tmp);
-        }
+            if (head2 == null) {
+                curr.next = head1;
+                break;
+            }
 
-        while (!heap.isEmpty()) {
-            ListNode min = heap.poll();
-            curr.next = min;
-            curr = curr.next;
-
-            if (min.next != null)
-                heap.offer(min.next);
+            // if both of then are not null, add smaller one into result
+            if (head1.val < head2.val) {
+                curr.next = head1;
+                head1 = head1.next;
+                curr = curr.next;
+            } else {
+                curr.next = head2;
+                head2 = head2.next;
+                curr = curr.next;
+            }
         }
 
         return dummy.next;
