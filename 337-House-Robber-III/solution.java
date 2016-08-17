97f838c91b27b51786d4aa5d68947a3a14fe0e4d
adam lee <adambillylee@gmail.com>
2016-08-17 01:20:44 -0700

    [337. House Robber III][Accepted]committed by LeetCode Extension

---
+++ 337-House-Robber-III/solution.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    class ReturnType {
+        int rob;
+        int notRob;
+
+        public ReturnType() {
+        }
+
+        public ReturnType(int rob, int notRob) {
+            this.rob = rob;
+            this.notRob = notRob;
+        }
+    }
+
+    public int rob(TreeNode root) {
+        ReturnType rst = dfs(root);
+
+        return Math.max(rst.rob, rst.notRob);
+    }
+
+    private ReturnType dfs(TreeNode root) {
+        if (root == null)
+            return new ReturnType(0, 0);
+
+        ReturnType left = dfs(root.left);
+        ReturnType right = dfs(root.right);
+
+        // rob case: max of left not rob + right not rob + current value
+        ReturnType rst = new ReturnType();
+        rst.rob = left.notRob + right.notRob + root.val;
+
+        // not rob case: max of left + max of right, both side can either be robbed or not
+        int leftMax = Math.max(left.notRob, left.rob);
+        int rightMax = Math.max(right.notRob, right.rob);
+        rst.notRob = leftMax + rightMax;
+
+        return rst;
+    }
+}
