dc4603fc21e2b1698e5e1cb80e3b710ad10a7039
adam lee <adambillylee@gmail.com>
2016-08-21 00:34:35 -0700

    [36. Valid Sudoku][Accepted]committed by LeetCode Extension

--- 36-Valid-Sudoku/solution.java
+++ 36-Valid-Sudoku/solution.java
@@ -4,63 +4,23 @@ import java.util.Arrays;
  * Created by adamli on 8/20/16.
  */
 public class Solution {
+    private boolean[][] horizontal = new boolean[9][9];
+    private boolean[][] vertical = new boolean[9][9];
+    private boolean[][] square = new boolean[9][9];
+    
     public boolean isValidSudoku(char[][] board) {
-        if (board == null || board.length == 0 || board[0].length == 0)
-            return false;
-
-        boolean nums[] = new boolean[9];
-
-        // check each rows
         for (int i = 0; i < 9; i++) {
-            Arrays.fill(nums, false);
             for (int j = 0; j < 9; j++) {
-                char c = board[i][j];
+                if (board[i][j] == '.')
+                    continue;
 
-                if (c != '.') {
-                    if (nums[c - '1']) {
-                        return false;
-                    } else {
-                        nums[c - '1'] = true;
-                    }
-                }
-            }
-        }
+                int index = (i / 3) * 3 + (j / 3);
+                int num = board[i][j] - '1';
 
-        // check each columns
-        for (int j = 0; j < 9; j++) {
-            Arrays.fill(nums, false);
-            for (int i = 0; i < 9; i++) {
-                char c = board[i][j];
+                if (horizontal[i][num] || vertical[j][num] || square[index][num])
+                    return false;
 
-                if (c != '.') {
-                    if (nums[c - '1']) {
-                        return false;
-                    } else {
-                        nums[c - '1'] = true;
-                    }
-                }
-            }
-        }
-
-        // check cube
-        // use i and j to loop across each cube
-        for (int i = 0; i < 9; i += 3) {
-            for (int j = 0; j < 9; j += 3) {
-                Arrays.fill(nums, false);
-
-                // use x and y to loop inside each cube
-                for (int x = 0; x < 3; x++) {
-                    for (int y = 0; y < 3; y++) {
-                        char c = board[i + x][j + y];
-                        if (c != '.') {
-                            if (nums[c - '1']) {
-                                return false;
-                            } else {
-                                nums[c - '1'] = true;
-                            }
-                        }
-                    }
-                }
+                horizontal[i][num] = vertical[j][num] = square[index][num] = true;
             }
         }
 
