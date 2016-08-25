54e0ea63158a7e24fc18b979ef88acf05817f276
adam lee <adambillylee@gmail.com>
2016-08-24 23:37:02 -0700

    [68. Text Justification][Accepted]committed by LeetCode Extension

--- 68-Text-Justification/solution.java
+++ 68-Text-Justification/solution.java
@@ -1,91 +1,29 @@
 public class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> result = new ArrayList<>();
-        if (words == null || words.length == 0 || maxWidth < 0) {
-            return result;
-        }
-         
-        if (maxWidth == 0) {
-            result.add("");
-            return result;
-        }
-         
-        fullJustifyHelper(0, words, result, maxWidth);
-         
-        return result;
-    }
-     
-    private void fullJustifyHelper(int start, String[] words, 
-                  List<String> result, int L) {
-        if (start >= words.length) {
-            return;
-        }
-         
-        int total = 0;
-        int len = 0;
-        int next = -1;
-        int i = start;
-         
-        while (i < words.length && total < L) {
-            total += words[i].length();
-             
-            if (total > L) {
-                next = i;
-                break;
+        List<String> list = new LinkedList<String>();
+
+        for (int i = 0, w; i < words.length; i = w) {
+            int len = -1;
+            for (w = i; w < words.length && len + words[w].length() + 1 <= maxWidth; w++) {
+                len += words[w].length() + 1;
             }
-             
-            len += words[i].length();
-            total++;
-            i++;
-        }
-         
-        if (next == -1) {
-            next = i;
-        }
-         
-        addLists(words, start, next, result, len, L);
-         
-        fullJustifyHelper(next, words, result, L);
-    }
-     
-    private void addLists(String[] words, int start, int next, 
-                          List<String> result, int len, int L) {
-        int slots = next - start - 1;
-        StringBuffer sb = new StringBuffer();
-        // Last line or only one word in a line
-        if (slots == 0 || next == words.length) {
-            for (int i = start; i < next; i++) {
-                sb.append(words[i]);
-                if (i == next - 1) {
-                    break;
-                }
-                sb.append(" ");
+
+            StringBuilder strBuilder = new StringBuilder(words[i]);
+            int space = 1, extra = 0;
+            if (w != i + 1 && w != words.length) { // not 1 char, not last line
+                space = (maxWidth - len) / (w - i - 1) + 1;
+                extra = (maxWidth - len) % (w - i - 1);
             }
-             
-            int trailingSpace = L - len - slots;
-            for (int i = 0; i < trailingSpace; i++) {
-                sb.append(" ");
+            for (int j = i + 1; j < w; j++) {
+                for (int s = space; s > 0; s--) strBuilder.append(' ');
+                if (extra-- > 0) strBuilder.append(' ');
+                strBuilder.append(words[j]);
             }
-             
-            result.add(sb.toString());
-        } else {
-            int aveSpace = (L - len) / slots;
-            int moreSpace = (L - len) % slots;
-            for (int i = start; i < next; i++) {
-                sb.append(words[i]);
-                if (i == next - 1) {
-                    break;
-                }
-                for (int j = 0; j < aveSpace; j++) {
-                    sb.append(" ");
-                }
-                 
-                if (moreSpace > 0) {
-                    sb.append(" ");
-                    moreSpace--;
-                }
-            }   
-            result.add(sb.toString());
+            int strLen = maxWidth - strBuilder.length();
+            while (strLen-- > 0) strBuilder.append(' ');
+            list.add(strBuilder.toString());
         }
+
+        return list;
     }
 }
