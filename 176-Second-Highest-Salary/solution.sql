d5b79a6fe62e9cf56eec46456c0b950a188db376
adam lee <adambillylee@gmail.com>
2016-08-28 00:11:03 -0700

    [176. Second Highest Salary][Accepted]committed by LeetCode Extension

--- 176-Second-Highest-Salary/solution.sql
+++ 176-Second-Highest-Salary/solution.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
 select max(t1.Salary) as SecondHighestSalary
-from (select e1.Salary from Employee e1 join Employee e2 on e1.Salary < e2.Salary group by e1.Id having count(e2.Id) = 1) t1;
+from (select e1.Salary, count(e1.id) as rank from Employee e1, Employee e2 where e2.Salary > e1.Salary group by e1.id) t1
+where rank = 1;
