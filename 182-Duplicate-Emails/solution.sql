6c4e5cc84dd1eb299b6f14f26c8407459a27409b
adam lee <adambillylee@gmail.com>
2016-08-27 22:55:36 -0700

    [182. Duplicate Emails][Accepted]committed by LeetCode Extension

--- 182-Duplicate-Emails/solution.sql
+++ 182-Duplicate-Emails/solution.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
-select Email
-from Person
-group by Email
-having count(Email) > 1;
+# Write your MySQL query statement
+select distinct(p1.Email)
+from Person p1, Person p2
+where p1.Email = p2.Email
+and p1.Id != p2.Id
